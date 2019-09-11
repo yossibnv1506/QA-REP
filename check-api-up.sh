9f11fd692731a71c4018da39b16801a45484bc3a
yossibnv <yossibnv@gmail.com>
2019-09-11 21:21:07 +0300

    updated file2

---
+++ check-api-up.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+ STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://www.google.com)
+  if [ $STATUS -eq 200 ]; then
+    echo "Got 200! All done!"
+#    exit 0 
+  else
+    echo "Got $STATUS :( Api Server not up yet..."
+    exit 1
+  fi
+  
+
+url =$1
+echo $url
+ip = $2
+echo $ip 
+username= sort -R /usr/share/dict/words | head -1
+review =  'hello'$username
+STATUS =$(curl -v -H  "Content-Type: application/json" -X POST --data '{$username ,$review}' -o /dev/null -w '%{http_code}' $url$ip)
+ if [ $STATUS -eq 200 ]; then
+    echo "Got 200! All done!"
+#    exit 0 
+  else
+    echo "Got $STATUS :( Information  Not saved ..."
+    exit 1
+  fi
+
+
