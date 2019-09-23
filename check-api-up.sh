ba5666dc4389d181019f053dd1a8c49d96572ae1
yossibnv1506 <50920404+yossibnv1506@users.noreply.github.com>
2019-09-23 14:32:35 +0300

    Update check-api-up.sh

--- check-api-up.sh
+++ check-api-up.sh
@@ -6,15 +6,15 @@ STATUS=$(curl -s -o /dev/null -w '%{http_code}' $url$ip)
     echo "Got 200! All done!"
 #    exit 0 
   else
-    echo "Got $STATUS :( Api Server not up yet..."
+    echo "Got $STATUS :( DB Server not up yet..."
     exit 1
   fi
   
 
 
 username= sort -R /usr/share/dict/words | head -1
-review =  'hello'$username
-STATUS =$(curl -v -H  "Content-Type: application/json" -X POST --data '{$username ,$review}' -o /dev/null -w '%{http_code}' $url$ip)
+text =  'hello '$RANDOM
+JsonRes=$(curl -X POST -H "Content-Type:application/json" http://localhost:3000/postMessage -d '{"userId":"1","msg":$text}')
  if [ $STATUS -eq 200 ]; then
     echo "Got 200! All done!"
 #    exit 0 
